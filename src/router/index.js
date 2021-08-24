c8556a7caadfb29a27ed5ded55574d7a9c2dc2ff
Lonely-Journey <854866731@qq.com>
2021-08-24 18:16:16 +0800

    2021-8-24 18:24

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import cinemaRouter from './Cinema/cinema.js'
+import mineRouter from './Mine/mine.js'
+import movieRouter from './Movie/movie.js'
 
 Vue.use(VueRouter)
 
 const routes = [
+  cinemaRouter,
+  mineRouter,
+  movieRouter,
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    redirect: '/movie'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*', // *是通配符，优先级是最低的
+    redirect: '/movie'
   }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  // 路由模式  一共有两种：hash  和  history
+  // 不写mode的情况下，默认是hash模式
+  // hash 的模式下网站格式      http://location/:8080/#/xxxx/xxxx
+  // history 的模式下网站格式   http://localhost:8080/xxxx/xxxx
+  // mode: 'history',
+
+  mode: 'hash',
+  base: process.env.BASE_URL,     // 根路径设置
+
+  // 如果配置变量的名字是routes的话这里就可以简写为routes
   routes
 })
 
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵"/>
+    <div id="content">
+      <Login />
+    </div>
+    <TabBar/>
+  </div>
+</template>
+
+<script>
+import Header from '../../components/Header/header.vue'
+import TabBar from '../../components/TabBar/tabBar.vue'
+import Login from '../../components/Login/Login.vue'
+
+export default {
+  name: 'Mine',
+  components: {
+    Header,
+    TabBar,
+    Login
+  }
+}
+</script>
+
+<style>
+</style>
